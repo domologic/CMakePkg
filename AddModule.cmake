6ba56a6c89af7062de3b892cb8db76bb15f086da
komelov <eugen.komelov@web.de>
2019-06-07 16:41:31 +0200

    Core: corrected threads library on unix

--- AddModule.cmake
+++ AddModule.cmake
@@ -167,10 +167,14 @@ macro(_add_module_link_libraries)
       set(LIBRARIES_INTERFACE "INTERFACE;${LIBRARY_LIST_INTERFACE}")
     endif()
 
+    find_package(Threads REQUIRED)
+
     target_link_libraries(${module_name}
       PUBLIC
         ${LIBRARY_LIST_PUBLIC}
         ${DEPS}
+        Threads::Threads
+        "$<$<COMPILE_LANGUAGE:GNU>:-lstdc++fs>"
       ${LIBRARIES_PRIVATE}
       ${LIBRARIES_INTERFACE}
     )
