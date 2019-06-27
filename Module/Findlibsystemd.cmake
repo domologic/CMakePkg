fd4440d704aab1baf27fb6ed2958ca487272f7d3
komelov <eugen.komelov@web.de>
2019-06-27 16:25:39 +0200

    Modules: replaced dbus with systemd

---
+++ Module/Findlibsystemd.cmake
@@ -0,0 +1,48 @@
+include(FindPackageHandleStandardArgs)
+
+find_package(PkgConfig QUIET)
+pkg_check_modules(PC_DBUS libsystemd QUIET)
+
+set(LIBSYSTEMD_VERSION ${PC_LIBSYSTEMD_VERSION})
+
+find_path(LIBSYSTEMD_INCLUDE_DIR
+  NAMES
+    systemd/sd-daemon.h
+  HINTS
+    ${PC_LIBSYSTEMD_INCLUDEDIR}
+    ${PC_LIBSYSTEMD_INCLUDE_DIRS}
+)
+
+find_library(LIBSYSTEMD_LIBRARY
+  NAMES
+    systemd
+  HINTS
+    ${PC_LIBSYSTEMD_LIBDIR}
+    ${PC_LIBSYSTEMD_LIBRARY_DIRS}
+)
+
+find_package_handle_standard_args(libsystemd
+  DEFAULT_MSG
+  LIBSYSTEMD_VERSION
+  LIBSYSTEMD_INCLUDE_DIR
+  LIBSYSTEMD_CONFIG_INCLUDE_DIR
+  LIBSYSTEMD_LIBRARY
+)
+
+if (libsystemd_FOUND)
+  set(LIBSYSTEMD_INCLUDE_DIRS
+    ${LIBSYSTEMD_INCLUDE_DIR}
+  )
+
+  set(LIBSYSTEMD_LIBRARIES
+    ${LIBSYSTEMD_LIBRARY}
+  )
+
+  mark_as_advanced(
+    LIBSYSTEMD_VERSION
+    LIBSYSTEMD_INCLUDE_DIR
+    LIBSYSTEMD_INCLUDE_DIRS
+    LIBSYSTEMD_LIBRARY
+    LIBSYSTEMD_LIBRARIES
+  )
+endif()
