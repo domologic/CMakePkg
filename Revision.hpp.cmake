9aba825a43f2a70e0d5413c0e88cf7018ee89650
komelov <eugen.komelov@web.de>
2019-09-05 12:19:22 +0200

    Core: generate module revision header from git information

---
+++ Revision.hpp.cmake
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifndef @MODULE_NAME@_REVISION
+#  define @MODULE_NAME@_REVISION  @MODULE_REVISION@
+#endif
+
+#ifndef @MODULE_NAME@_TIMESTAMP
+#  define @MODULE_NAME@_TIMESTAMP @MODULE_TIMESTAMP@
+#endif
+
+#ifndef @MODULE_NAME@_BRANCH
+#  define @MODULE_NAME@_BRANCH    @MODULE_BRANCH@
+#endif
