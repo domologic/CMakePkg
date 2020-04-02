1bbb54693615f60dcf9b901a28ab6e4faaa3a96b
komelov <eugen.komelov@web.de>
2020-04-02 13:37:39 +0200

    Modules: added find_package script for libudev

---
+++ Module/Findlibudev.cmake
@@ -0,0 +1,53 @@
+#
+# find_package module for libudev library
+#
+# Tries to locate the libudev library with pkgconfig
+#
+
+include(FindPackageHandleStandardArgs)
+
+find_package(PkgConfig QUIET)
+pkg_check_modules(PC_LIBUDEV libudev QUIET)
+
+set(LIBUDEV_VERSION ${PC_LIBUDEV_VERSION})
+
+find_path(LIBUDEV_INCLUDE_DIR
+  NAMES
+    libudev.h
+  HINTS
+    ${PC_LIBUDEV_INCLUDEDIR}
+    ${PC_LIBUDEV_INCLUDE_DIRS}
+)
+
+find_library(LIBUDEV_LIBRARY
+  NAMES
+    libudev
+  HINTS
+    ${PC_LIBUDEV_LIBDIR}
+    ${PC_LIBUDEV_LIBRARY_DIRS}
+)
+
+find_package_handle_standard_args(libudev
+  DEFAULT_MSG
+  LIBUDEV_VERSION
+  LIBUDEV_INCLUDE_DIR
+  LIBUDEV_LIBRARY
+)
+
+if (libudev_FOUND)
+  set(LIBUDEV_INCLUDE_DIRS
+    ${LIBUDEV_INCLUDE_DIR}
+  )
+
+  set(LIBUDEV_LIBRARIES
+    ${LIBUDEV_LIBRARY}
+  )
+
+  mark_as_advanced(
+    LIBUDEV_VERSION
+    LIBUDEV_INCLUDE_DIR
+    LIBUDEV_INCLUDE_DIRS
+    LIBUDEV_LIBRARY
+    LIBUDEV_LIBRARIES
+  )
+endif()
