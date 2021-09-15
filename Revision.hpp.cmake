e5d964c3b5eb88774a2e65f3cb5ba4b4076807fa
Gerrit Telkamp <g.telkamp@domologic.de>
2021-09-15 06:28:56 +0200

    * renamed: DOMAIN/REPOSITORY * adding MODULE_TAG * Comments

--- Revision.hpp.cmake
+++ Revision.hpp.cmake
@@ -3,6 +3,7 @@
 namespace @MODULE_NAME@::revision {
     constexpr const char* kVersion   = "@MODULE_VERSION@";
     constexpr const char* kRevision  = "@MODULE_REVISION@";
+    constexpr const char* kTag       = "@MODULE_TAG@";
     constexpr const char* kTimestamp = "@MODULE_TIMESTAMP@";
     constexpr const char* kDate      = "@MODULE_DATE@";
     constexpr int         kYear      = @MODULE_YEAR@;
