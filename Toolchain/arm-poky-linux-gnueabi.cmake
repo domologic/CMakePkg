0723fc04ef4a78af6c55418e34fa1457ef9a490e
komelov <eugen.komelov@web.de>
2021-09-15 15:06:26 +0200

    updated toolchain flags

--- Toolchain/arm-poky-linux-gnueabi.cmake
+++ Toolchain/arm-poky-linux-gnueabi.cmake
@@ -4,7 +4,7 @@
 
 set(TOOLCHAIN         "/opt/yocto-sdk/sigma/sysroots/x86_64-pokysdk-linux/usr/bin/arm-poky-linux-gnueabi/arm-poky-linux-gnueabi")
 set(TOOLCHAIN_SYSROOT "/opt/yocto-sdk/sigma/sysroots/cortexa7t2hf-neon-poky-linux-gnueabi")
-set(TOOLCHAIN_FLAGS   "-march=armv7-a -mfloat-abi=hard -mcpu=cortex-a9 -mfpu=neon-vfpv3 -mtune=cortex-a9 -mabi=aapcs-linux -mvectorize-with-neon-quad -Wno-psabi")
+set(TOOLCHAIN_FLAGS   "-marm -mlittle-endian -mtune=cortex-a7 -mcpu=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard -mtp=auto -mabi=aapcs-linux -mvectorize-with-neon-quad -Wno-psabi")
 
 set(CMAKE_SYSTEM_NAME      Linux                 CACHE STRING "" FORCE)
 set(CMAKE_SYSTEM_PROCESSOR arm                   CACHE STRING "" FORCE)
