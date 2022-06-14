06b879ad7a0396416c6b6a3893f0cdcff1b2efc1
komelov <eugen.komelov@web.de>
2022-06-14 15:11:31 +0200

    updated the revision.hpp for backwards compatibility

--- Revision.hpp.cmake
+++ Revision.hpp.cmake
@@ -5,4 +5,38 @@ namespace @MODULE_NAME@ {
     constexpr auto kTimestamp = "@MODULE_TIMESTAMP@";
     constexpr auto kDate      = "@MODULE_DATE@";
     constexpr auto kYear      = @MODULE_YEAR@;
+
+    /**
+     * DEPRECATED
+     */
+    namespace revision {
+        /**
+         * DEPRECATED
+         */
+        [[deprecated]] constexpr auto kVersion   = "@MODULE_VERSION@";
+        /**
+         * DEPRECATED
+         */
+        [[deprecated]] constexpr auto kRevision  = "@MODULE_REVISION@";
+        /**
+         * DEPRECATED
+         */
+        [[deprecated]] constexpr auto kTag       = "@MODULE_TAG@";
+        /**
+         * DEPRECATED
+         */
+        [[deprecated]] constexpr auto kTimestamp = "@MODULE_TIMESTAMP@";
+        /**
+         * DEPRECATED
+         */
+        [[deprecated]] constexpr auto kDate      = "@MODULE_DATE@";
+        /**
+         * DEPRECATED
+         */
+        [[deprecated]] constexpr auto kYear      = @MODULE_YEAR@;
+        /**
+         * DEPRECATED
+         */
+        [[deprecated]] constexpr auto kBranch    = "@MODULE_BRANCH@";
+    }
 }
