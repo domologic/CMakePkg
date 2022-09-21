55aedbf17b45609a6f29379cfc705e1672d2e28e
komelov <eugen.komelov@web.de>
2022-09-21 13:00:31 +0200

    minor cleanup in scripts

--- Scripts/GenCommitIdFiles.py
+++ Scripts/GenCommitIdFiles.py
@@ -1,34 +1,33 @@
-import argparse
-import os
-import os.path
-import git
-import time
+from argparse import ArgumentParser
+from git import Repo
+from os import mkdir, walk
+from os.path import exists, join, splitext
 
-def writeTagfile(repo, depsdir, commit, tagfile):
+def write_tag_file(repo, deps_dir, commit, tag_file):
     packages = {}
-    for subdir, dirs, files in os.walk(depsdir):
+    for dir, dirs, files in walk(deps_dir):
         for d in dirs:
-            depsrepo           = git.Repo(os.path.join(subdir, d))
-            url                = depsrepo.remotes.origin.url.split(':/')[-1]
-            package_id         = os.path.splitext(url)[0].split('/');
+            package_id         = splitext(url)[0].split('/')
             package_id.pop(0)
             package_id.pop(0)
+            deps_repo          = Repo(join(dir, d))
+            url                = deps_repo.remotes.origin.url.split(':/')[-1]
             package            = '/'.join(package_id)
-            packages[package]  = depsrepo.git.rev_list('-1', f'--before="{commit.authored_date}"', 'HEAD')
+            packages[package]  = deps_repo.git.rev_list('-1', f'--before="{commit.authored_date}"', 'HEAD')
         break
 
-    print(f"Creating tagfile '{commit}'")
-    with open(tagfile, 'w') as f:
-        print('---COMMITID BEGIN---\n')
-        for package, commitid in sorted(packages.items()):
-            if commitid:
-                f.write(f"{package}: {commitid}\n")
-        print('---COMMITID END---')
+    print(f"Creating tag_file '{commit}'")
+    with open(tag_file, 'w') as f:
+        f.write('---COMMITID BEGIN---\n')
+        for package, commit_id in sorted(packages.items()):
+            if commit_id:
+                f.write(f"{package}: {commit_id}\n")
+        f.write('---COMMITID END---')
 
-repo = git.Repo(".")
+repo = Repo('.')
 
 def parse_args():
-    parser = argparse.ArgumentParser(usage='Generates tag files for given commit range.')
+    parser = ArgumentParser(usage='Generates tag files for given commit range.')
     parser.add_argument('--path',  required=True, help='Path to the build folder')
     parser.add_argument('--start', required=True, help='First commit')
     parser.add_argument('--end',   required=True, help='Last commit')
@@ -37,8 +36,8 @@ def parse_args():
 
 args = parse_args();
 
-if not os.path.exists(args.out):
-    os.mkdir(args.out)
+if not oexists(args.out):
+    mkdir(args.out)
 
 for commit in repo.iter_commits(rev=f'{args.start}..{args.end}'):
-    writeTagfile(repo, f'{args.path}/deps', commit, f'{args.out}/{commit.hexsha}')
+    write_tag_file(repo, f'{args.path}/deps', commit, f'{args.out}/{commit.hexsha}')
