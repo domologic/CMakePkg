b6bb3f8f8a9f408875c747763196f69019a1b93d
Eugen Komelev <e.komelev@domologic.de>
2024-08-21 17:23:40 +0200

    improved git version detection to allow proper semver

--- Revision.hpp.cmake
+++ Revision.hpp.cmake
@@ -1,42 +1,95 @@
 #pragma once
 
 namespace @PACKAGE_NAME@ {
-    constexpr auto kVersion      = "@PACKAGE_VERSION@";
+    /**
+     * Version of the @PACKAGE_NAME@ package.
+     */
+    constexpr auto kVersion         = "@PACKAGE_VERSION@";
 
 #cmakedefine PACKAGE_VERSION_MAJOR
 #ifdef PACKAGE_VERSION_MAJOR
-    constexpr auto kVersionMajor = @PACKAGE_VERSION_MAJOR@;
+    /**
+     * Major version of the @PACKAGE_NAME@ package.
+     */
+    constexpr auto kVersionMajor    = @PACKAGE_VERSION_MAJOR@;
 #else
-    constexpr auto kVersionMajor = 0;
+    /**
+     * Major version of the @PACKAGE_NAME@ package.
+     */
+    constexpr auto kVersionMajor    = 0;
 #endif
 #undef PACKAGE_VERSION_MAJOR
 
 #cmakedefine PACKAGE_VERSION_MINOR
 #ifdef PACKAGE_VERSION_MINOR
-    constexpr auto kVersionMinor = @PACKAGE_VERSION_MINOR@;
+    /**
+     * Minor version of the @PACKAGE_NAME@ package.
+     */
+    constexpr auto kVersionMinor    = @PACKAGE_VERSION_MINOR@;
 #else
-    constexpr auto kVersionMinor = 0;
+    /**
+     * Minor version of the @PACKAGE_NAME@ package.
+     */
+    constexpr auto kVersionMinor    = 0;
 #endif
 #undef PACKAGE_VERSION_MINOR
 
 #cmakedefine PACKAGE_VERSION_PATCH
 #ifdef PACKAGE_VERSION_PATCH
-    constexpr auto kVersionPatch = @PACKAGE_VERSION_PATCH@;
+    /**
+     * Patch version of the @PACKAGE_NAME@ package.
+     */
+    constexpr auto kVersionPatch    = @PACKAGE_VERSION_PATCH@;
 #else
-    constexpr auto kVersionPatch = 0;
+    /**
+     * Patch version of the @PACKAGE_NAME@ package.
+     */
+    constexpr auto kVersionPatch    = 0;
 #endif
 #undef PACKAGE_VERSION_PATCH
 
 #cmakedefine PACKAGE_VERSION_TWEAK
 #ifdef PACKAGE_VERSION_TWEAK
-    constexpr auto kVersionTweak = @PACKAGE_VERSION_TWEAK@;
+    /**
+     * Tweak version of the @PACKAGE_NAME@ package.
+     */
+    constexpr auto kVersionTweak    = @PACKAGE_VERSION_TWEAK@;
 #else
-    constexpr auto kVersionTweak = 0;
+    /**
+     * Tweak version of the @PACKAGE_NAME@ package.
+     */
+    constexpr auto kVersionTweak    = 0;
 #endif
 #undef PACKAGE_VERSION_TWEAK
 
-    constexpr auto kTimestamp = "@PACKAGE_TIMESTAMP@";
-    constexpr auto kDate      = "@PACKAGE_DATE@";
-    constexpr auto kTime      = "@PACKAGE_TIME@";
-    constexpr auto kYear      = @PACKAGE_YEAR@;
+#cmakedefine PACKAGE_VERSION_COMMIT_ID
+#ifdef PACKAGE_VERSION_COMMIT_ID
+    /**
+     * Commit id of the @PACKAGE_NAME@ package.
+     */
+    constexpr auto kVersionCommitId = "@PACKAGE_VERSION_COMMIT_ID@";
+#else
+    /**
+     * Commit id of the @PACKAGE_NAME@ package.
+     */
+    constexpr auto kVersionCommitId = "";
+#endif
+#undef PACKAGE_VERSION_COMMIT_ID
+
+    /**
+     * Timestamp of the @PACKAGE_NAME@ package.
+     */
+    constexpr auto kTimestamp       = "@PACKAGE_TIMESTAMP@";
+    /**
+     * Date of the @PACKAGE_NAME@ package.
+     */
+    constexpr auto kDate            = "@PACKAGE_DATE@";
+    /**
+     * Time of the @PACKAGE_NAME@ package.
+     */
+    constexpr auto kTime            = "@PACKAGE_TIME@";
+    /**
+     * Year of the @PACKAGE_NAME@ package.
+     */
+    constexpr auto kYear            = @PACKAGE_YEAR@;
 }
