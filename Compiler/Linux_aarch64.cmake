643e13b2947b2e082ffad176f6316e130c24e7ef
Eugen Komelev <e.komelev@domologic.de>
2026-05-05 16:55:55 +0200

    Compiler: added aarch64 configuration

---
+++ Compiler/Linux_aarch64.cmake
@@ -0,0 +1,39 @@
+#
+# Internal
+# Load compiler configuration for Linux arm platform
+#
+
+set_compiler_defaults(
+  # debug preprocessor definitions
+  DEFINE_DEBUG
+    DEBUG
+    _DEBUG
+  # release preprocessor definitions
+  DEFINE_RELEASE
+    NDEBUG
+    _NDEBUG
+  # compiler flags
+  FLAGS
+    -fno-gnu-tm
+    -fuse-ld=gold
+    -ftrivial-auto-var-init=zero
+  # debug compiler flags
+  FLAGS_DEBUG
+    -ggdb3
+    -O0
+  # release compiler flags
+  FLAGS_RELEASE
+    -g3
+    -O2
+    -fdata-sections
+    -ffunction-sections
+    -flto=auto
+    -fmerge-all-constants
+    -funroll-loops
+    -fwhole-program
+  # linker flags
+  LINK
+    LINKER:--gc-sections
+    LINKER:--disable-linker-version
+    LINKER:-z noexecstack
+)
